0f83533b74833eed9ac947b76d1acd6bd7640e7b
Jack Rayburn <jackrayburn@ip-192-168-86-105.ec2.internal>
2020-12-08 20:56:09 -0600

    read in data, started part1

--- Day3/day_3.py
+++ Day3/day_3.py
@@ -1,1 +1,24 @@
 ## tree problem
+## read in data
+with open('input.txt') as f:
+	data = f.read().splitlines()
+
+# get length of each row
+for i in data:
+    len(i) 
+# 31
+# 3 right, 1 down
+
+# track position across data
+pos = 0
+for row in data:
+    tree = []
+    pos += 3
+    if row[pos] == '#':
+        tree.append(1)
+        print(tree)
+    else:
+        continue
+
+
+    
