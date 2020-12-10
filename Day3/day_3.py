8115f772646bf9fd1d0d016a64d4b08b456405ce
Jack Rayburn <jackrayburn@ip-192-168-86-105.ec2.internal>
2020-12-10 10:26:02 -0600

    still broke

--- Day3/day_3.py
+++ Day3/day_3.py
@@ -9,17 +9,28 @@ for i in data:
 # 31
 # 3 right, 1 down
 del data[0]
-print(data[0])
 # track position across data
 pos = 0
+tree = 0
+dat = 1
 for row in data:
-    tree = []
-    pos += 3
-    if row[pos] == '#':
-        tree.append(1)
-        print(tree)
+    print(dat)
+    print(tree)
+    if pos == 30:
+        pos = 2
+    elif pos == 29:
+        pos = 1
+    elif pos == 28:
+        pos = 0
     else:
-        continue
-
+        pos += 3
+    if row[pos] == '#':
+        tree += 1
+    
+    print(row[pos])
+    print(pos)
+    print("end")
+    dat += 1
+    
 
     
